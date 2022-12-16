9e605e54d4025770f0b8ae6385acddb20c900e95
DucHLDev <duchuynhdev@gmail.com>
2022-12-16 15:35:47 +0700

    commit mern-ecommerce-project

---
+++ backend/dataPosttest.js
@@ -0,0 +1,18 @@
+// {
+//     "first_name":"Duc",
+//     "last_name": "Le",
+//     "gender":"m",
+//     "email":"test2@gmail.com",
+//     "password":"123",
+//     "number":84783888756,
+// }
+
+// {
+//     "customer_name": "Huy4",
+//     "customer_gender" : "Nam",
+//     "customer_email": "Huy4@gmail.com",
+//     "customer_address": "Ly Tu Trong",
+//     "customer_phone": "078388756",
+//     "customer_username": "Admin",
+//     "customer_password": "Admin"
+// }
